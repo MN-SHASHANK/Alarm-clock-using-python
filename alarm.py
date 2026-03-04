3fcd6395079cb4b5400beb3eae2b98d90c1ce770
MN-SHASHANK <mnshashanknag@gmail.com>
2026-03-04 15:21:35 +0530

    fixed the code

--- alarm.py
+++ alarm.py
@@ -26,6 +26,6 @@ def set_alarm(alarm_time):
         time.sleep(1)
 
 
-if _name_ == "__main__":
+if __name__ == "__main__":
     alarm_time = input("Enter the alarm time (HH:MM:SS): ")
     set_alarm(alarm_time)
